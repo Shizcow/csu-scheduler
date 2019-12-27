4c8ca1d6fd00f67a25767956287e74850668d63c
Shizcow <pohl.devin@gmail.com>
2019-12-27 13:27:54 -0700

    Changed server to port 80. Lots more work on the backend.

--- config.js
+++ config.js
@@ -189,7 +189,7 @@ app_config.useCredentials = false;
  * @memberOf app_config
  * @constant
  */
-app_config.URLprefix = 'http://shizcow-pi.ddns.net:3000/csu-crawler?';
+app_config.URLprefix = 'http://shizcow-pi.ddns.net/csu-crawler?';
 
 /**
  * Before we continue, it's time to introcude the GETPOST object
